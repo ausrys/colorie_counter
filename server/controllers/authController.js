10d7908e659bcc2ad2553977d1b6d013fb2fbd2f
ausrys <ausryszz1@gmail.com>
2023-11-06 17:48:13 +0200

    authcontroller try catch

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -34,21 +34,23 @@ module.exports.post_login = async (req, res) => {
     return res.status(400).json({ error: "All values must be entered" });
   const existingUser = await User.findOne({ where: { user_email: email } });
   if (!existingUser) return res.status(400).json("User doesn't exist");
-  bcrypt.compare(password, existingUser.user_password).then((isMatching) => {
-    if (!isMatching)
-      res.status(400).json({ error: "Wrong username or password" });
-    else {
-      const accessToken = createTokens(existingUser);
-      const { user_password, ...rest } = existingUser.dataValues;
-      res.cookie("accessToken", accessToken, {
-        httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24 * 30,
-        sameSite: "none",
-        secure: true,
-      });
-      res.status(200).json({ ...rest });
-    }
-  });
+  try {
+    bcrypt.compare(password, existingUser.user_password).then((isMatching) => {
+      if (!isMatching)
+        res.status(400).json({ error: "Wrong username or password" });
+      else {
+        const accessToken = createTokens(existingUser);
+        const { user_password, ...rest } = existingUser.dataValues;
+        res.cookie("accessToken", accessToken, {
+          httpOnly: true,
+          maxAge: 1000 * 60 * 60 * 24 * 30,
+          sameSite: "none",
+          secure: true,
+        });
+        res.status(200).json({ ...rest });
+      }
+    });
+  } catch (error) {}
 };
 // Only for testing
 module.exports.get_cookie = async (req, res) => {
